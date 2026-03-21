4683db62416faea4db9bce022f813a670362f52c
alexandreluchetti <luchetti.92@gmail.com>
2026-03-21 11:38:12 -0300

    Creating UserResponse.java

---
+++ src/main/java/br/com/alexandreluchetti/cinealert/core/model/user/UserResponse.java
@@ -0,0 +1,44 @@
+package br.com.alexandreluchetti.cinealert.core.model.user;
+
+public class UserResponse {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String avatarUrl;
+    private long totalReminders;
+    private long sentReminders;
+
+    public UserResponse(Long id, String name, String email, String avatarUrl, long totalReminders, long sentReminders) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.avatarUrl = avatarUrl;
+        this.totalReminders = totalReminders;
+        this.sentReminders = sentReminders;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public long getTotalReminders() {
+        return totalReminders;
+    }
+
+    public long getSentReminders() {
+        return sentReminders;
+    }
+}
